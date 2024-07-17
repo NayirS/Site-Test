d7f626e9a6c5a3927d378e70d5346e869d88f83c
Nayir <Nayir@exemple.fr>
2024-07-17 17:04:20 +0200

    nouveau

--- math.php
+++
@@ -1,9 +0,0 @@
-<?php
- // ceci est un commentaire
-function addition ($a , $b){
-
-    return $a + $b;
-}
-echo 'Bonjour , le monde ' , addition( 1 , 1) ; 
-//include './" nom de mon fichier dans le dossier'
-?>
